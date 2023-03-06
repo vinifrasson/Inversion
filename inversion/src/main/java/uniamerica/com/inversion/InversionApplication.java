66d0398ace44d6e7d4c3cdd00fc05706277dc1fd
Vinícius Frasson <vinifrasson13@gmail.com>
2023-03-06 19:24:49 -0300

    SpringBoot

---
+++ inversion/src/main/java/uniamerica/com/inversion/InversionApplication.java
@@ -0,0 +1,13 @@
+package uniamerica.com.inversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InversionApplication.class, args);
+	}
+
+}
---
+++ inversion/src/test/java/uniamerica/com/inversion/InversionApplicationTests.java
@@ -0,0 +1,13 @@
+package uniamerica.com.inversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
